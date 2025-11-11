55876400e52007785fa6fe799d754c6b319ea963
leirdan <uouleirdan@gmail.com>
2025-11-10 21:00:04 -0300

    refact: split traversal algorithms

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,6 @@ mod minimum_spanning_tree;
 mod shortest_path;
 mod traversal;
 
-pub use graph::BfsEvent;
-pub use graph::DfsEvent;
-pub use graph::Edge;
 pub use graph::Graph;
 pub use graph::Node;
 pub use graph::UndirectedGraph;
@@ -16,4 +13,11 @@ pub use graph::UndirectedGraph;
 pub mod graphs {
     pub use crate::adjacency_matrix::AdjacencyMatrix;
     pub use crate::shortest_path::DijkstraIter;
+    pub use crate::traversal::BfsEvent;
+    pub use crate::traversal::BfsIter;
+    pub use crate::traversal::BiconnectedComponentsIter;
+    pub use crate::traversal::DfsEdgesIter;
+    pub use crate::traversal::DfsEvent;
+    pub use crate::traversal::DfsIter;
+    pub use crate::traversal::Edge;
 }
